b98069b1677a9441861ccf8385f67b958a3e01a2
Brian Semrad <bjsemrad@gmail.com>
2025-06-10 21:50:35 -0500

    inital hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,10 +14,33 @@ dnf5 install -y tmux
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enable ublue-os/staging
+
+dnf5 -y copr enable ublue-os/staging
+
+
+dnf5 -y install solopasha/hyprland
+
+
+dnf5 -y install erikreider/SwayNotificationCenter
+
+
+dnf5 install -y --setopt=install_weak_deps=False \
+    xdg-desktop-portal-hyprland \
+    hyprland \
+    hyprpaper \
+    hyprland-qtutils \
+    hyprpolkitagent \
+    waybar \
+    wdisplays \
+    pavucontrol \
+    hyprlock \
+    hypridle \
+    NetworkManager-tui \
+    uwsm
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 
