1e74f2585d4e14b23e5c848e74544e368f0e2d13
Brian Semrad <bjsemrad@gmail.com>
2025-06-15 11:50:38 -0500

    add nix -- don't start daemon

--- build_files/build.sh
+++ build_files/build.sh
@@ -61,6 +61,6 @@ dnf5 -y copr disable hazel-bunny/ricing
 
 #### Example for enabling a System Unit File
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm --no-start-daemon
 
 systemctl enable podman.socket
