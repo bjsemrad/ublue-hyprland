c81ce0668dc61582c1924b68ce6a15251c7a97cb
Brian Semrad <bjsemrad@gmail.com>
2025-06-18 20:45:13 -0500

    nix destdir

--- build_files/build.sh
+++ build_files/build.sh
@@ -67,11 +67,7 @@ export NIX_STATE_DIR=/var/lib/nix/var
 export NIX_CONF_DIR=/var/lib/nix/etc
 
 # Set install location
-# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate
-
-sh <(curl -L https://nixos.org/nix/install) --daemon --yes --destdir /var/lib/nix
-
-#curl -L https://nixos.org/nix/install | bash -s install --daemon --yes --destdir /var/lib/nix
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --daemon --no-confirm --destdir /var/lib/nix
 
 # Add environment to skeleton so new users get it
 cat << EOF > /etc/profile.d/nix.sh
